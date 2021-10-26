d42232d419a2bb316c7ffddc05af24b7cc75ffc6
nyagajr <muchokijnrbenson@gmail.com>
2021-10-26 13:45:47 +0300

    Adding components

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.scss'
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='wrapper'>
+                <div className='left'>
+                    <a href="#intro"> My logo </a>
+                </div>
+                <div className='right'></div>
+            </div>
+            
+        </div>
+    )
+}
