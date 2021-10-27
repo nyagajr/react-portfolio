a04112733b1a6ab57485d18ab2b75856d2d38e06
nyagajr <muchokijnrbenson@gmail.com>
2021-10-27 11:42:28 +0300

    adding humburger menu functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Contact from './components/contact/Contact';
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
-import './app.scss'
+import './app.scss';
+import {useState} from 'react'
 
 
 function App() {
+  const[humOpen,setHumOpen] = useState(false)
   return (
     <div className="app">
-     <Navbar />
+     <Navbar humOpen={humOpen} setHumOpen={setHumOpen}/>
      <div className="sections">
       <Intro/>
       <Portfolio/>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import './navbar.scss'
-
+import {ContactPhone, Mail} from '@material-ui/icons'
 export default function Navbar() {
     return (
         <div className='navbar'>
             <div className='wrapper'>
                 <div className='left'>
-                    <a href="#intro"> My logo </a>
+                    <a href="#intro" className='logo'> Nyaga </a>
+                    <div className="itemContainer">
+                        <ContactPhone className='icon'/>
+                        <span>+254717106233</span>
+                    </div>
+
+                    <div className="itemContainer">
+                        <Mail className='icon'/>
+                        <span>nyagaben123@gmail.com</span>
+                    </div>
+                        
+                    </div>
                 </div>
-                <div className='right'></div>
+                <div className='right'>
+                    <div className="hamburger">
+                        <span className='line1'></span>
+                        <span className='line3'></span>
+                        <span className='line3'></span>
+                    </div>
+                </div>
             </div>
             
-        </div>
+        
     )
 }
