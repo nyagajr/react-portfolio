dedc83976b81bcd0b8dd71ef07165bbe5f3b3ed0
nyagajr <muchokijnrbenson@gmail.com>
2021-10-28 13:33:49 +0300

    adding content on intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,12 @@ import './intro.scss'
 export default function Intro() {
     return (
         <div className='intro' id='intro'>
-            
+            <div className="left">
+                <div className="dPic">
+                <img src={process.env.PUBLIC_URL + '/assets/nyagz.png'} />
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
