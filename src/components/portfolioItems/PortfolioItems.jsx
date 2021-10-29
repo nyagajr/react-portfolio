5398807b4cd48f4d150d8b75408ddfb4e6d84d26
nyagajr <muchokijnrbenson@gmail.com>
2021-10-29 18:35:18 +0300

    adding a new component

---
+++ src/components/portfolioItems/PortfolioItems.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './portfolioItems.scss'
+
+export default function PortfolioItems() {
+    return (
+        <li className='portfolioItems'>
+            
+        </li>
+    )
+}
