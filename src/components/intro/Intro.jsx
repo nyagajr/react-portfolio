7edd850cd924cb66f025538d75be369a84b3aa33
nyagajr <muchokijnrbenson@gmail.com>
2021-10-29 18:30:49 +0300

    finishing up on Introduction section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './intro.scss'
 import {ArrowDownwardTwoTone as ArrowDownwardTwoToneIcon} from '@material-ui/icons';
+import { init } from 'ityped'
+import { useEffect, useRef } from'react'
 
 export default function Intro() {
+    const headerTxt = useRef()
+    useEffect(() => {
+        init(headerTxt.current, 
+            { showCursor: false, 
+                typeSpeed:  300,
+              backDelay:  90000,
+              strings: ['Benson Nyaga' ] })
+    }, [])
+
+    const tickerTxt = useRef()
+    useEffect(() => {
+        init(tickerTxt.current, 
+            { showCursor: true, 
+                typeSpeed:  100,
+                startDelay: 5000,
+              backDelay:  1000,
+              strings: ['Web Development','Back-end Development', 'Front-end Development' ] })
+    }, [])
+
     return (
         <div className='intro' id='intro'>
             <div className="left">
@@ -10,11 +31,13 @@ export default function Intro() {
                 <img src={process.env.PUBLIC_URL + '/assets/nyagz.png'} />
                 </div>
             </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Holla, I'm</h2>
-                    <h1>Benson Nyaga</h1>
-                    <h3>A Software Developer.</h3>
+
+            <div className='right'>
+                <div className="wrapper" >
+                    <h2>Holla, I am</h2>
+                    <h1 ref={headerTxt}></h1>
+                    <h3>Software Developer</h3>
+                    <h4 className='typer'>Specialized in: <b><span ref={tickerTxt}></span></b></h4>
                 </div>
 
                 <a href="#portfolio">
