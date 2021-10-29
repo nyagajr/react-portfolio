be36f457df4a025db67d2a86241a35d0734aabc3
nyagajr <muchokijnrbenson@gmail.com>
2021-10-29 18:33:55 +0300

    adding a portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,45 @@ import './portfolio.scss'
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
+            <h1>Portfolio</h1>
+            <ul>
+                <li className='active'>Featured</li>
+                <li>Web App</li>
+                <li>Front-end App</li>
+                <li>Back-end App</li>
+            </ul>
            
+           <div className="container">
+               <div className="item">
+               <img src={process.env.PUBLIC_URL + '/assets/png-clipart-security-hacker-computer-security-certified-ethical-hacker-white-hat-hacker-tshirt-computer-network-thumbnail.png'} />
+                   <h3>Dev Enthausiast Website</h3>
+               </div>
+
+               <div className="item">
+               <img src={process.env.PUBLIC_URL + '/assets/png-clipart-security-hacker-computer-security-certified-ethical-hacker-white-hat-hacker-tshirt-computer-network-thumbnail.png'} />
+                   <h3>Dev Enthausiast Website</h3>
+               </div>
+
+               <div className="item">
+               <img src={process.env.PUBLIC_URL + '/assets/png-clipart-security-hacker-computer-security-certified-ethical-hacker-white-hat-hacker-tshirt-computer-network-thumbnail.png'} />
+                   <h3>Dev Enthausiast Website</h3>
+               </div>
+
+               <div className="item">
+               <img src={process.env.PUBLIC_URL + '/assets/png-clipart-security-hacker-computer-security-certified-ethical-hacker-white-hat-hacker-tshirt-computer-network-thumbnail.png'} />
+                   <h3>Dev Enthausiast Website</h3>
+               </div>
+
+               <div className="item">
+               <img src={process.env.PUBLIC_URL + '/assets/png-clipart-security-hacker-computer-security-certified-ethical-hacker-white-hat-hacker-tshirt-computer-network-thumbnail.png'} />
+                   <h3>Dev Enthausiast Website</h3>
+               </div>
+
+               <div className="item">
+               <img src={process.env.PUBLIC_URL + '/assets/png-clipart-security-hacker-computer-security-certified-ethical-hacker-white-hat-hacker-tshirt-computer-network-thumbnail.png'} />
+                   <h3>Dev Enthausiast Website</h3>
+               </div>
+           </div>
         </div>
     )
 }
